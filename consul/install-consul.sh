aa8a0c35f3c1fa43638b4ad5a53a548777e40c51
jasonluck <jluck@meetveracity.com>
2017-08-11 14:07:40 -0400

    Add consul-client ami

--- consul/install-consul.sh
+++ consul/install-consul.sh
@@ -9,4 +9,16 @@ sudo chmod 0755 /usr/bin/consul
 sudo chown -R root:root /usr/bin/consul
 rm consul.zip
 
+# Configure Consul
+sudo mkdir /etc/consul.d
+sudo mv ~/consul-config.json /etc/consul.d/config.json
+sudo chown -R root:root /etc/consul.d
+sudo chmod -R 0700 /etc/consul.d
+
+sudo mkdir /opt/consul
+sudo mkdir /opt/consul/data
+sudo mkdir /opt/consul/config
+sudo chown -R root:root /opt/consul
+sudo chmod -R 0700 /opt/consul
+
 
