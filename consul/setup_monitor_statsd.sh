337254f3123cc01c86885078509fefabf2721759
Cloud User <centos@ip-172-31-31-56.ec2.internal>
2018-02-08 20:18:30 +0000

    updated to not create prometheus dir b/c base AMI does that

--- consul/setup_monitor_statsd.sh
+++ consul/setup_monitor_statsd.sh
@@ -1,16 +1,6 @@
 #!/bin/bash -ex
 
-
-
-sudo mkdir -p /apps/prometheus/
-
-sudo chown -R ec2-user:ec2-user /apps
-
 cd /apps/prometheus/
-
-
-
-## we got node scraper from curl -LO "https://github.com/prometheus/node_exporter/releases/download/0.12.0rc3/node_exporter-0.12.0rc3.linux-amd64.tar.gz"
 
 curl -LO "https://github.com/prometheus/statsd_exporter/releases/download/v0.6.0/statsd_exporter-0.6.0.linux-amd64.tar.gz"
 
