ef00096a08458e39b6d76a39d997c5a11a5e48fa
jasonluck <jluck@meetveracity.com>
2018-04-16 15:21:30 -0400

    Removing vault ami files that are no longer needed

--- vault/prereqs.sh
+++
@@ -1,5 +0,0 @@
-# update
-sudo yum -y update
-
-# Install uzip utility
-sudo yum -y install unzip
--- vault/vault-redirectaddr.sh
+++
@@ -1,2 +0,0 @@
-export PRIVATE_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-export VAULT_REDIRECT_ADDR="https://$PRIVATE_IP:8200"
