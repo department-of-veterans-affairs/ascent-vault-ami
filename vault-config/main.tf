8c64afd7ce79b6b4fe42f25efba324e7d5319406
jasonluck <jluck@meetveracity.com>
2018-05-07 12:21:36 -0400

    Adding roles

--- vault-config/main.tf
+++ vault-config/main.tf
@@ -88,3 +88,97 @@ resource "vault_policy" "sonar" {
     name = "sonar"
     policy = "${file("${path.module}/policy/sonar.hcl")}"
 }
+
+###############################################################################
+#
+# Vault Roles
+#
+###############################################################################
+
+resource "vault_generic_secret" "role_ascent_platform" {
+  path = "auth/token/roles/ascent-platform"
+
+  data_json = <<EOT
+{
+  "role_name": "ascent-platform",
+  "allowed_policies": "ascent-app,ascent-config,ascent-dashboard,ascent-zipkin,ascent-gateway"
+}
+EOT
+}
+
+resource "vault_generic_secret" "role_ascent_demo" {
+  path = "auth/token/roles/ascent-demo"
+
+  data_json = <<EOT
+{
+  "role_name": "ascent-demo",
+  "allowed_policies": "ascent-app,ascent-demo"
+}
+EOT
+}
+
+resource "vault_generic_secret" "role_refdata" {
+  path = "auth/token/roles/refdata"
+
+  data_json = <<EOT
+{
+  "role_name": "refdata",
+  "allowed_policies": "ascent-app,refdata"
+}
+EOT
+}
+
+resource "vault_generic_secret" "role_refdata" {
+  path = "auth/token/roles/refdata"
+
+  data_json = <<EOT
+{
+  "role_name": "refdata",
+  "allowed_policies": "ascent-app,refdata"
+}
+EOT
+}
+
+resource "vault_generic_secret" "role_intenttofile" {
+  path = "auth/token/roles/intenttofile"
+
+  data_json = <<EOT
+{
+  "role_name": "intenttofile",
+  "allowed_policies": "ascent-app,intenttofile"
+}
+EOT
+}
+
+resource "vault_generic_secret" "role_redis" {
+  path = "auth/token/roles/redis"
+
+  data_json = <<EOT
+{
+  "role_name": "redis",
+  "allowed_policies": "redis"
+}
+EOT
+}
+
+resource "vault_generic_secret" "role_elk" {
+  path = "auth/token/roles/elk"
+
+  data_json = <<EOT
+{
+  "role_name": "elk",
+  "allowed_policies": "elasticsearch,kibana"
+}
+EOT
+}
+
+resource "vault_generic_secret" "role_ci" {
+  path = "auth/token/roles/ci"
+
+  data_json = <<EOT
+{
+  "role_name": "ci",
+  "allowed_policies": "jenkins-ci,sonar,nexus"
+}
+EOT
+}
