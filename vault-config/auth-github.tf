3bf312682d5c04cbbd1a98291a66c4d566e17d35
jasonluck <jluck@meetveracity.com>
2018-05-07 10:48:44 -0400

    Setup github authentication

---
+++ vault-config/auth-github.tf
@@ -0,0 +1,34 @@
+###############################################################################
+#
+# Setup Authentication with Vault using Github
+# Access policies are based on team membership within the organization
+#
+###############################################################################
+
+resource "vault_auth_backend" "github" {
+  type = "github"
+}
+
+resource "vault_generic_secret" "github_dsva" {
+  path = "auth/github/config"
+
+  data_json = <<EOT
+{
+  "organization": "department-of-veterans-affairs"
+}
+EOT
+}
+
+#------------------------------------------------------
+# Map team membership to roles
+#------------------------------------------------------
+
+resource "vault_generic_secret" "github_devops" {
+  path = "auth/github/map/teams/ascent-devops"
+
+  data_json = <<EOT
+{
+  "value": "devops"
+}
+EOT
+}
