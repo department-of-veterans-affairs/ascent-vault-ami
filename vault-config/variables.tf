3a7c65b5b5c976e803570b407dacace759de4117
jasonluck <jluck@meetveracity.com>
2018-05-08 21:53:01 -0400

    Adding all application secrets

---
+++ vault-config/variables.tf
@@ -0,0 +1,30 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "git_access_token" {
+  description = "Github access token with access to external configuration repository"
+}
+
+variable "sonar_git_access_token" {
+  description = "Github access token Sonar will use to comment on Github Pull Requests"
+}
+
+variable "jwt_secret_token" {
+  description = "Pre-shared JWT access token used to authentication clients."
+}
+
+variable "bgs_password" {
+  description = "Password to be used by our micro-services to authenticate with BGS services. To be supplied by BGS."
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults, but can be overriden to customize this module.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "bgs_username" {
+  description = "Username to be used by our micro-services to authenticate with BGS services. To be supplied by BGS."
+  default = ""
+}
