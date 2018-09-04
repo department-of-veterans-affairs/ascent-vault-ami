647d4fcad316a203a8c7fe90281dce521e0dcf76
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-09-04 11:14:05 -0500

    Get rid of the aws backend altogether as a resource and as an argument

--- vault-config/add_role_auth_for_ssl/main.tf
+++ vault-config/add_role_auth_for_ssl/main.tf
@@ -1,9 +1,5 @@
-resource "vault_auth_backend" "aws" {
-  type = "aws"
-}
 
 resource "vault_aws_auth_backend_role" "instance_backend_role" {
-  backend              = "${vault_auth_backend.aws.path}"
   role                 = "vetservices"
   auth_type            = "ec2"
   bound_iam_instance_profile_arn = "${var.bound_profile_arn}"
