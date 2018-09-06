53b43652e39eef330a0fcf9d4be5b3eb3a34eb27
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-09-06 12:22:32 -0500

    delete inferred_entity_type

--- vault-config/add_role_auth_for_ssl/main.tf
+++ vault-config/add_role_auth_for_ssl/main.tf
@@ -3,7 +3,6 @@ resource "vault_aws_auth_backend_role" "instance_backend_role" {
   role                 = "vetservices"
   auth_type            = "ec2"
   bound_iam_instance_profile_arn = "${var.bound_profile_arn}"
-  inferred_entity_type = "ec2_instance"
   ttl                  = 60
   max_ttl              = 120
   policies             = ["ssl_certificates"]
