577f94984f1ce7e04e510b6894ecd7e89bdec895
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-09-06 13:21:43 -0500

    reorder the roles, and have only one backend client

--- vault-config/aws-auth.tf
+++ vault-config/aws-auth.tf
@@ -2,12 +2,20 @@ resource "vault_auth_backend" "aws" {
   type = "aws"
 }
 
-resource "vault_aws_auth_backend_client" "proxy" {
+resource "vault_aws_auth_backend_client" "client" {
   backend = "${vault_auth_backend.aws.path}"
 }
 
-resource "vault_aws_auth_backend_client" "docker" {
-  backend = "${vault_auth_backend.aws.path}"
+resource "vault_aws_auth_backend_role" "docker" {
+  backend              = "${vault_auth_backend.aws.path}"
+  role                 = "vetservices"
+  auth_type            = "ec2"
+  bound_iam_role_arn   = "${var.docker_role_arn}"
+  ttl                  = 60
+  max_ttl              = 120
+  policies             = ["${vault_policy.sign-csr.name}", "${vault_policy.ssl_certificates.name}"]
+
+  depends_on = ["vault_aws_auth_backend_client.client"]
 }
 
 resource "vault_aws_auth_backend_role" "proxy" {
@@ -19,17 +27,5 @@ resource "vault_aws_auth_backend_role" "proxy" {
   max_ttl              = 120
   policies             = ["${vault_policy.ssl_certificates.name}"]
 
-  depends_on = ["vault_aws_auth_backend_client.proxy"]
-}
-
-resource "vault_aws_auth_backend_role" "docker" {
-  backend              = "${vault_auth_backend.aws.path}"
-  role                 = "vetservices"
-  auth_type            = "ec2"
-  bound_iam_role_arn   = "${var.docker_role_arn}"
-  ttl                  = 60
-  max_ttl              = 120
-  policies             = ["${vault_policy.ssl_certificates.name}", "${vault_policy.sign-csr.name}"]
-
-  depends_on = ["vault_aws_auth_backend_client.docker"]
+  depends_on = ["vault_aws_auth_backend_client.client"]
 }
