78083561ea6b69f50d7b1d5015be7bc5890c1234
jasonluck <jluck@meetveracity.com>
2018-09-21 10:21:54 -0400

    fixing error in proxy role

--- vault-config/aws-auth.tf
+++ vault-config/aws-auth.tf
@@ -11,7 +11,7 @@ resource "vault_aws_auth_backend_role" "proxy" {
   backend                        = "${vault_auth_backend.aws.path}"
   role                           = "vetservices"
   auth_type                      = "ec2"
-  bound_iam_role_arn             = ["${var.role_arn}"]
+  bound_iam_role_arn             = "${var.role_arn}"
   inferred_entity_type           = "ec2_instance"
   ttl                            = 60
   max_ttl                        = 120
