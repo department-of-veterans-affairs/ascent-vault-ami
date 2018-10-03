a48244ac74a2105ee7d7dfefaab75e2844f2fa81
jasonluck <jluck@meetveracity.com>
2018-10-03 15:09:09 -0400

    Try using role arn

--- vault-config/add_role_auth_for_ssl/main.tf
+++ vault-config/add_role_auth_for_ssl/main.tf
@@ -2,7 +2,7 @@
 resource "vault_aws_auth_backend_role" "instance_backend_role" {
   role                 = "vetservices"
   auth_type            = "ec2"
-  bound_iam_instance_profile_arn = "${var.bound_profile_arn}"
+  bound_iam_role_arn = "${var.bound_profile_arn}"
   ttl                  = 60
   max_ttl              = 120
   policies             = ["ssl_certificates"]
